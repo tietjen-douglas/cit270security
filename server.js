9ed43b769225a878b44707c7d194d578f7c6e6ab
Douglas Tietjen <tietjend@byui.edu>
2022-05-14 15:24:18 -0600

    Implementing Redis as DB

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 const express = require('express'); // Import the library
 const bodyParser = require('body-parser'); // Middleware
+const md5 = require('md5');
+const redis = require("redis")
+const redisClient = redis.createClient();
+
 const app = express(); // Use the library
 const port = 3000;
 
 app.use(bodyParser.json()); // Use the middleware (call it before anything else happens on the request)
 
+(async() => {
+    await redisClient.connect();
+
+    redisClient.on('error', (err) => console.log('Redis Client Error', err));
+})
+
 // Listen
-app.listen(port, () => {
-    console.log(`Listening on Port ${port}`)
+app.listen(port, async () => {
+     console.log(`Listening on Port ${port}`)
 });
 
 // Respond
-app.get('/', (req, res) => {
-    res.send('Hello')
+app.get('/', async (req, res) => {
+     res.send('Hello')
 });
 
-app.post('/login', (request, response) => {
+app.post('/login', async (request, response) => {
     const loginRequest = request.body;
-    if (loginRequest.userName == 'perkley@hotmail.com' && loginRequest.password == 'Abc123!') {
+    const hashedPasswordFromUser = md5(loginRequest.password);
+    
+    var hashedPasswordFromDB = await redisClient.hGet('passwords', loginRequest.userName, function(err, reply){
+        console.log('hashed in DB:', reply);
+    });
+
+    if (loginRequest.userName == 'perkley@hotmail.com' && hashedPasswordFromUser == hashedPasswordFromDB) {
         response.status(200);
-        response.send("Welcome.");
+        response.send(`Welcome.`);
     } else {
         response.status(401); // Not authorized
         response.send("Unauthorized!");
